2417aba839caa5a851cb7caae4b2204d8d7d70dd
byTheInK <un.known.river.person@gmail.com>
2025-01-23 19:06:23 +0300

    load

--- main.py
+++ main.py
@@ -2,7 +2,27 @@ import ollama
 import vosk
 from gtts import gTTS
 from subprocess import run
-import re
+from sys import stdout as terminal
+from time import sleep
+from itertools import cycle
+from threading import Thread
+from os import system
+
+done = False
+    
+def load():
+    global done
+    print("Yükleniyor: ", end="")
+
+    for c in cycle(['|', '/', '-', '\\']):
+        if done:
+            break
+
+        terminal.write(f'\rYükleniyor: {c}')
+        terminal.flush()
+        sleep(0.15)
+
+    terminal.flush()
 
 def pull(model="gemma2"):
     print(f"{model.capitalize()} modeli bulunamadı")
@@ -17,25 +37,38 @@ def pull(model="gemma2"):
     return 
 
 def main():
+    global done
+
     while True:
+        done = False
         message = input()
 
         try:
+            Thread(target=load).start()
+
             response: ollama.ChatResponse = ollama.chat(
                 model="gemma2",
                 messages=[{"role": "user", "content": message}],
-                )
+            )
 
+            done = True 
+
+            system("clear")
+            print()
             response = response.message.content
                 
             print(response, end="", flush=True)
+            print()
 
             gTTS(text=response,lang="tr",slow=False).save("sounds/tts.mp3")
             run(["cvlc", "--play-and-exit", "sounds/tts.mp3"])
 
         except ollama.ResponseError as e:
             if e.status_code == 404:
-                pull("gemma2")
+                pull()
+                return
+        except Exception as e:
+            print("Bir hata oluştu: \n", e)
 
 if __name__ == "__main__":
     main()
